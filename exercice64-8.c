96a759e8ee7efb2c2f72861d03a6bd228db658cf
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-02-28 17:12:33 +0100

    Example of how floats don't have an absolute precision in C

---
+++ exercice64-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char **argv) {
+  float f = 0.0;
+  int i;
+  printf("%.12f,\n", f);
+  for (i = 0; i < 10; i++) {
+    f += 0.1;
+    printf("%.12f,\n", f);
+  }
+  //printf("%.12f, %.12f\n", f, 1.);
+  if (f==1.0) printf("f egal 1.0\n");
+  else printf("f different de 1.0\n");
+  return 0;
+}
